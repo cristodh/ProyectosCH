f2a5aa5a813da3f0f6d3fbe08e66fe622c6869ed
Barry Vega <bvegac@est.utn.ac.cr>
2025-08-06 15:35:22 -0600

    Actualizacion

--- prueba.js
+++ prueba.js
@@ -1,3 +1,12 @@
 console.log ("Hola");
 console.log("Este es un proyecto de prueba en JavaScript.");
 console.log("¡Listo para empezar a programar!");
+
+
+if (3>4) {
+    console.log("Verdadero");
+    
+}else{
+    console.log("Falso");
+    
+}
